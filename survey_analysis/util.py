cdde4e0cf24c1004fcf88eaf70c447037bbd88f4
Norman Ziegner <norman.ziegner@ufz.de>
2020-04-29 16:17:01 +0200

    Pass filter arguments using kwargs

--- survey_analysis/util.py
+++ survey_analysis/util.py
@@ -13,17 +13,17 @@ from .question import Question
 
 def filter_and_group(
         filter_question: Question,
-        filter_args,
-        group_question: Question
+        group_question: Question,
+        **filter_args
         ) -> Dict[Answer, Dict[str, List[Answer]]]:
     """
     Obtain filtered results grouped by the answers of a question.
 
     Args:
         filter_question: The question whose given answers are to be filtered.
-        filter_args:     Arguments for the filter
         group_question:  The question according to whose given answers the
                          participants are grouped.
+        filter_args:     Arguments passed to filter.
 
     Returns:
         An association of answers of grouped_question to the filtered answers
@@ -35,8 +35,9 @@ def filter_and_group(
         dict.fromkeys(grouped_answers)
 
     for answer, participant_ids in grouped_answers.items():
+        filter_args['participant_id'] = participant_ids
         results[answer] = filter_question.filter_given_answers(
-            participant_id=participant_ids
+            **filter_args
         )
 
     return results
