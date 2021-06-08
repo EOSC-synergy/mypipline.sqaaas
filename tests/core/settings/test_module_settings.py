8f3be96e07a21c09b82b88413931a851f96ac5d1
Christian Hueser <c.hueser@hzdr.de>
2021-06-08 10:32:05 +0200

    Remove test cases that test module settings and refine the others
    * Some test cases make not much sense and can be removed.
    * Add some comments next to assert statements in test cases.
    * Remove setting run timestamp explicitly, because it is already set.

--- tests/core/settings/test_module_settings.py
+++ tests/core/settings/test_module_settings.py
@@ -55,6 +55,7 @@ class TestModuleSettings(object):
         """
         expected_verbosity: int = logging.NOTSET
         settings: Settings = settings_fixture
+        # Assure that initial verbosity is NOTSET.
         assert settings.VERBOSITY == expected_verbosity, \
                "Default value of verbosity is not correct."
 
@@ -71,6 +72,7 @@ class TestModuleSettings(object):
         verbosity_count: int = 2
         settings: Settings = settings_fixture
         settings.set_verbosity(verbosity_count)
+        # Assure that verbosity set is as expected.
         assert settings.VERBOSITY == expected_verbosity, \
                "Value of verbosity set is not correct."
 
@@ -86,48 +88,9 @@ class TestModuleSettings(object):
         expected_timestamp_regex: re = \
             r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}'
         settings: Settings = settings_fixture
-        settings.set_timestamp('')
+        # Assure that timestamp is matching the timestamp pattern.
         assert re.match(expected_timestamp_regex, settings.RUN_TIMESTAMP), \
                "Format of default value of run timestamp is not correct."
-
-    @pytest.mark.ci
-    def test_setting_run_timestamp_works(self, settings_fixture: Settings):
-        """
-        Tests that setting run timestamp works fine.
-
-        Args:
-            settings_fixture (Settings):
-                New Settings object containing all settings of an analysis run.
-        """
-        expected_timestamp_regex: re = \
-            r'\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}'
-        arbitrary_timestamp: str = "2021-06-07_12-34-56"
-        settings: Settings = settings_fixture
-        settings.RUN_TIMESTAMP = arbitrary_timestamp
-        assert re.match(expected_timestamp_regex, settings.RUN_TIMESTAMP), \
-            "Format of run timestamp set is not as expected."
-
-    @pytest.mark.ci
-    def test_analysis_output_path_is_correct(self, settings_fixture: Settings):
-        """
-        Tests that analysis output path default value is correct.
-
-        Args:
-            settings_fixture (Settings):
-                New Settings object containing all settings of an analysis run.
-        """
-        settings: Settings = settings_fixture
-        expected_output_folder: str = "output"
-        expected_run_timestamp: str = settings.set_timestamp('')
-        expected_output_folder_path: Path = \
-            Path(expected_output_folder) / Path(expected_run_timestamp)
-        settings.assemble_output_path(
-            '',
-            {"OUTPUT_FOLDER": Path(expected_output_folder),
-             "RUN_TIMESTAMP": Path(expected_run_timestamp)})
-        assert settings.ANALYSIS_OUTPUT_PATH.absolute() == \
-               Path(expected_output_folder_path).absolute(), \
-               "Default value of analysis output path is not correct."
 
     @pytest.mark.ci
     def test_output_format_is_correct(self, settings_fixture: Settings):
@@ -141,6 +104,7 @@ class TestModuleSettings(object):
         expected_output_format: SupportedOutputFormat = \
             SupportedOutputFormat.SCREEN
         settings: Settings = settings_fixture
+        # Assure that initial output format is SCREEN.
         assert settings.OUTPUT_FORMAT == expected_output_format, \
                "Default value of output format is not correct."
 
@@ -156,6 +120,7 @@ class TestModuleSettings(object):
         """
         expected_metadata_path: str = "metadata/metadata.yml"
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom metadata path is as expected.
         assert settings.METADATA.absolute() == \
                Path(expected_metadata_path).absolute(), \
                "Value of metadata path set is not correct."
@@ -172,6 +137,7 @@ class TestModuleSettings(object):
         """
         expected_output_folder: str = "output_folder"
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom output folder path is as expected.
         assert settings.OUTPUT_FOLDER.absolute() == \
                Path(expected_output_folder).absolute(), \
                "Value of output folder set is not correct."
@@ -189,6 +155,7 @@ class TestModuleSettings(object):
         expected_output_format: SupportedOutputFormat = \
             SupportedOutputFormat.SVG
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom output format is as expected.
         assert settings.OUTPUT_FORMAT == expected_output_format, \
                "Value of output format set is not correct."
 
@@ -204,6 +171,7 @@ class TestModuleSettings(object):
         """
         expected_script_folder: str = "scripts_folder"
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom scripts folder path is as expected.
         assert settings.SCRIPT_FOLDER.absolute() == \
                Path(expected_script_folder).absolute(), \
                "Value of scripts folder set is not correct."
@@ -219,6 +187,7 @@ class TestModuleSettings(object):
                 New Settings object containing all settings of an analysis run.
         """
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom scripts names is a list.
         assert isinstance(settings.SCRIPT_NAMES, List), \
                "Datatype of script names is not a list."
 
@@ -234,6 +203,7 @@ class TestModuleSettings(object):
         """
         expected_script_names_count: int = 1
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom list of script names contains one element.
         assert len(settings.SCRIPT_NAMES) == expected_script_names_count, \
                "List size of script names is not as expected."
 
@@ -249,5 +219,6 @@ class TestModuleSettings(object):
         """
         expected_script_name: str = "example_script"
         settings: Settings = settings_custom_config_fixture
+        # Assure that loaded custom script names contain expected script name.
         assert settings.SCRIPT_NAMES[0] == expected_script_name, \
                "First element of list of script names is not as expected."
